8cd7accce59a585244642cc9b1d39e6661556d63
unknown <derek.slauson5@gmail.com>
2022-05-02 20:51:22 -0500

    Removed cors

--- index.js
+++ index.js
@@ -13,10 +13,10 @@ app.use(cookieSession({
     keys: [process.env.SESSION_SECRET],
     maxAge: 24 * 60 * 60 * 1000 // 24 hours
 }))
-app.use(cors({
-    origin: "http://localhost:3000",
-    credentials: true
-}))
+// app.use(cors({
+//     origin: "http://localhost:3000",
+//     credentials: true
+// }))
 
 app.use(defineCurrentUser)
 
