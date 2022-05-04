830a3034d7f744abdb3df2b6bc9fb71efa1bfa68
unknown <derek.slauson5@gmail.com>
2022-05-04 18:37:16 -0500

    Connection method wasn't working so switched back

--- config/config.js
+++ config/config.js
@@ -16,14 +16,7 @@ module.exports = {
     "dialect": "mysql"
   },
   "production": {
-    "use_env_variable": "DATABASE_URL",
-    "dialect": "postgres",
-    "dialectOptions": {
-      "ssl": {
-        "require": true,
-        "rejectUnauthorized": false
-      }
-    }
+    "use_env_variable": "DATABASE_URL"
   }
 }
 
