3e1c92e16f4e1c7b424a2502cb8e339a66970b06
unknown <derek.slauson5@gmail.com>
2022-05-04 17:53:58 -0500

    Trying different connection method just to see if it works

--- config/config.js
+++ config/config.js
@@ -16,10 +16,12 @@ module.exports = {
     "dialect": "mysql"
   },
   "production": {
-    "username": process.env.DB_USERNAME,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_DATABASE,
-    "host": "127.0.0.1",
-    "dialect": process.env.DB_DIALECT
+    "use_env_variable": "DATABASE_URL"
   }
 }
+
+// "username": process.env.DB_USERNAME,
+// "password": process.env.DB_PASSWORD,
+// "database": process.env.DB_DATABASE,
+// "host": "127.0.0.1",
+// "dialect": process.env.DB_DIALECT
