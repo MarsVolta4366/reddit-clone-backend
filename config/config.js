425c30d42368752ffbfc60fb64fcbecd253ad5d0
unknown <derek.slauson5@gmail.com>
2022-05-04 17:13:55 -0500

    Changed production connection info in config.js

--- config/config.js
+++ config/config.js
@@ -16,10 +16,14 @@ module.exports = {
     "dialect": "mysql"
   },
   "production": {
-    "username": process.env.DB_USERNAME,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_DATABASE,
-    "host": "127.0.0.1",
-    "dialect": process.env.DB_DIALECT
+    "connectionString": process.env.DATABASE_URL
   }
 }
+
+// "production": {
+//   "username": process.env.DB_USERNAME,
+//   "password": process.env.DB_PASSWORD,
+//   "database": process.env.DB_DATABASE,
+//   "host": "127.0.0.1",
+//   "dialect": process.env.DB_DIALECT
+// }
