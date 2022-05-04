cd3b794ffcde53cd504bf497dc29783a2fadce11
unknown <derek.slauson5@gmail.com>
2022-05-04 17:41:07 -0500

    Changed config back, now changed index.js

--- config/config.js
+++ config/config.js
@@ -16,14 +16,10 @@ module.exports = {
     "dialect": "mysql"
   },
   "production": {
-    "connectionString": process.env.DATABASE_URL
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_DATABASE,
+    "host": "127.0.0.1",
+    "dialect": process.env.DB_DIALECT
   }
 }
-
-// "production": {
-//   "username": process.env.DB_USERNAME,
-//   "password": process.env.DB_PASSWORD,
-//   "database": process.env.DB_DATABASE,
-//   "host": "127.0.0.1",
-//   "dialect": process.env.DB_DIALECT
-// }
--- models/index.js
+++ models/index.js
@@ -9,11 +9,18 @@ const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
 
 let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+// if (config.use_env_variable) {
+//   sequelize = new Sequelize(process.env[config.use_env_variable], config);
+// } else {
+//   sequelize = new Sequelize(config.database, config.username, config.password, config);
+// }
+sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  dialectOptions: {
+    ssl: true
+  }
+});
 
 fs
   .readdirSync(__dirname)
